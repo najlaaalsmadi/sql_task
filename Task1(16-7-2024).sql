f66122a636e6031d73ea877d66a666e512166132
najlaaalsmadi <103289306+najlaaalsmadi@users.noreply.github.com>
2024-07-17 01:10:21 +0300

    Add files via upload

---
+++ Task1(16-7-2024).sql
@@ -0,0 +1,106 @@
+CREATE DATABASE Library;
+/*----------------------------------------*/
+USE Library;
+  CREATE Table Authors(
+  AuthorID INT PRIMARY KEY,
+  FirstName VARCHAR(35),
+  LastName VARCHAR(35),
+  BirthYear DATE,
+  
+  );
+
+USE Library;
+CREATE TABLE Books (
+    BookID INT NOT NULL,
+    Title VARCHAR(35),
+    PublishedYear INT,
+    CopiesAvailable INT,
+    BookCategory VARCHAR(45),
+    AuthorID INT,
+    PRIMARY KEY(BookID),
+    FOREIGN KEY (AuthorID) REFERENCES Authors (AuthorID)
+);
+
+SELECT * FROM Books;
+/**************************************************/
+INSERT INTO Books( [BookID], [Title], [PublishedYear], [CopiesAvailable], [BookCategory], [AuthorID])
+VALUES (1, 'A Beautiful and Respectful Book', 2022, 50, 'Children', 1);
+
+INSERT INTO Books( [BookID], [Title], [PublishedYear], [CopiesAvailable], [BookCategory], [AuthorID])
+VALUES (2, 'Learning is Fun', 2021, 45, 'Education', 2);
+
+INSERT INTO Books( [BookID], [Title], [PublishedYear], [CopiesAvailable], [BookCategory], [AuthorID])
+VALUES (3, 'Adventures in the Forest', 2013, 30, 'Adventure', 3);
+
+INSERT INTO Books( [BookID], [Title], [PublishedYear], [CopiesAvailable], [BookCategory], [AuthorID])
+VALUES (4, 'The Magic Garden', 2008, 25, 'Fantasy', 4);
+
+INSERT INTO Books( [BookID], [Title], [PublishedYear], [CopiesAvailable], [BookCategory], [AuthorID])
+VALUES (5, 'Under the Sea', 2004, 40, 'Nature', 5);
+
+INSERT INTO Books( [BookID], [Title], [PublishedYear], [CopiesAvailable], [BookCategory], [AuthorID])
+VALUES (6, 'Space Explorers', 2012, 35, 'Science Fiction', 6);
+
+INSERT INTO Books( [BookID], [Title], [PublishedYear], [CopiesAvailable], [BookCategory], [AuthorID])
+VALUES (7, 'Friendly Animals', 2016, 60, 'Animals', 7);
+
+INSERT INTO Books( [BookID], [Title], [PublishedYear], [CopiesAvailable], [BookCategory], [AuthorID])
+VALUES (8, 'Mystery Island', 2005, 20, 'Mystery', 8);
+
+INSERT INTO Books( [BookID], [Title], [PublishedYear], [CopiesAvailable], [BookCategory], [AuthorID])
+VALUES (9, 'Journey to the Past', 2009, 55, 'History', 9);
+
+INSERT INTO Books( [BookID], [Title], [PublishedYear], [CopiesAvailable], [BookCategory], [AuthorID])
+VALUES (10, 'The World of Dinosaurs', 2023, 50, 'Science', 10);
+
+
+INSERT INTO Authors ([AuthorID], [FirstName], [LastName], [BirthYear]) VALUES
+(1, 'ALI', 'AHMAD', '2005-05-02');
+
+INSERT INTO Authors ([AuthorID], [FirstName], [LastName], [BirthYear]) VALUES
+(2, 'SARA', 'KHAN', '2001-08-15');
+
+INSERT INTO Authors ([AuthorID], [FirstName], [LastName], [BirthYear]) VALUES
+(3, 'MOHAMMED', 'HASSAN', '1998-11-25');
+
+INSERT INTO Authors ([AuthorID], [FirstName], [LastName], [BirthYear]) VALUES
+(4, 'FATIMA', 'AL-SHAMI', '2000-03-10');
+
+INSERT INTO Authors ([AuthorID], [FirstName], [LastName], [BirthYear]) VALUES
+(5, 'OMAR', 'NAJI', '1999-07-30');
+
+INSERT INTO Authors ([AuthorID], [FirstName], [LastName], [BirthYear]) VALUES
+(6, 'AYESHA', 'JAMIL', '2002-12-05');
+
+INSERT INTO Authors ([AuthorID], [FirstName], [LastName], [BirthYear]) VALUES
+(7, 'KHALID', 'MANSOUR', '1997-04-22');
+
+INSERT INTO Authors ([AuthorID], [FirstName], [LastName], [BirthYear]) VALUES
+(8, 'LAILA', 'SADIQ', '2003-09-14');
+
+INSERT INTO Authors ([AuthorID], [FirstName], [LastName], [BirthYear]) VALUES
+(9, 'HUSSEIN', 'QADIR', '2001-06-18');
+
+INSERT INTO Authors ([AuthorID], [FirstName], [LastName], [BirthYear]) VALUES
+(10, 'NOURA', 'BAKR', '2004-01-29');
+
+ /***************************************************/
+SELECT MAX(CopiesAvailable)
+FROM Books;
+
+SELECT MIN(CopiesAvailable)
+FROM Books;
+/*****************************************************/
+DROP TABLE Authors;
+/*****************************************************/
+--ALTER TABLE Books 
+--ALTER COLUMN PublishedYear INT;
+SELECT AVG(PublishedYear) AS AveragePublishedYear
+FROM Books
+WHERE PublishedYear > 2000;
+
+/*****************************************************/
+SELECT COUNT(BookID)
+FROM Books;
+ /*************************************************************/
+ 
---
+++ task2(17-7-2024).sql
@@ -0,0 +1,125 @@
+create database School_System;
+use School_System;
+create table Teachers(
+TeacherID int PRIMARY KEY,
+FirstName VARCHAR (50),
+LastName VARCHAR (50),
+Subject VARCHAR (50),
+);
+iNSERT INTO Teachers ([TeacherID], [FirstName], [LastName], [Subject]) VALUES 
+(1, 'Doha', 'Al-smadi', 'Math');
+iNSERT INTO Teachers ([TeacherID], [FirstName], [LastName], [Subject]) VALUES 
+(12, 'Ali', 'Ahmed', 'Science'),
+(3, 'Sara', 'Mohamed', 'English'),
+(4, 'Omar', 'Khan', 'History'),
+(5, 'Laila', 'Ali', 'Geography'),
+(6, 'Nora', 'Hassan', 'Biology'),
+(7, 'Khalid', 'Farah', 'Chemistry'),
+(8, 'Fatima', 'Zahra', 'Physics'),
+(9, 'Samir', 'Jamal', 'Art'),
+(10, 'Maya', 'El-sayed', 'Music');
+
+use School_System;
+CREATE TABLE TeacherDetails(
+Salary FLOAT,
+TeacherID_Details INT PRIMARY KEY,
+Email VARCHAR (50),
+PHONE VARCHAR (50),
+Teacher_ID INT,
+FOREIGN KEY (Teacher_ID) REFERENCES  Teachers(TeacherID)
+
+);
+INSERT INTO TeacherDetails ([Salary], [TeacherID_Details], [Email], [PHONE], [Teacher_ID]) VALUES 
+(330, 1, 'hala@gmail.com', '077779583', 1),
+(450, 2, 'ali@gmail.com', '078888999', 2),
+(500, 3, 'sara@gmail.com', '079999000', 3),
+(400, 4, 'omar@gmail.com', '077777111', 4),
+(350, 5, 'laila@gmail.com', '076666222', 5),
+(600, 6, 'nora@gmail.com', '075555333', 6),
+(480, 7, 'khalid@gmail.com', '074444444', 7),
+(550, 8, 'fatima@gmail.com', '073333555', 8),
+(520, 9, 'samir@gmail.com', '072222666', 9),
+(300, 10, 'maya@gmail.com', '071111777', 10);
+
+CREATE TABLE Courses(
+CourseID INT PRIMARY KEY,
+CourseName VARCHAR(30),
+InstructorID INT,
+FOREIGN KEY( InstructorID ) REFERENCES  Teachers(TeacherID)
+);
+INSERT INTO Courses ([CourseID], [CourseName], [InstructorID]) VALUES 
+(330,'hala', 1),
+(450, 'ali', 2),
+(500, 'sara', 2),
+(400, 'omar', 4),
+(350,'laila', 5),
+(600,'nora', 3),
+(480, 'khalid', 8),
+(550,'fatima', 9);
+use School_System;
+CREATE TABLE Students(
+StudentsID INT PRIMARY KEY,
+FirstName VARCHAR (50),
+LastName VARCHAR (50),
+);
+INSERT INTO Students ([StudentsID], [FirstName], [LastName]) VALUES 
+(400, 'Hala', 'Ahmad'),
+(401, 'Ali', 'Mohamed'),
+(402, 'Sara', 'Khan'),
+(403, 'Omar', 'Abdallah'),
+(404, 'Laila', 'Zaid'),
+(405, 'Nora', 'El-sayed'),
+(406, 'Khalid', 'Farah'),
+(407, 'Fatima', 'Ali'),
+(408, 'Samir', 'Jamal'),
+(409, 'Maya', 'Hassan');
+
+CREATE TABLE Courses_Students(
+CourseID INT,
+StudentsID INT,
+PRIMARY KEY(CourseID,StudentsID),
+FOREIGN KEY (CourseID) REFERENCES Courses(CourseID),
+FOREIGN KEY (StudentsID) REFERENCES Students(StudentsID),
+
+);
+use School_System;
+INSERT INTO Courses_Students (CourseID, StudentsID) VALUES 
+(330, 400),
+(330, 401),
+(450, 402),
+(450, 403),
+(500, 404),
+(500, 405),
+(330, 406),
+(450, 407),
+(500, 408),
+(330, 409);
+
+
+CREATE TABLE Teachers_Students(
+TeacherID INT,
+StudentsID INT,
+PRIMARY KEY(TeacherID,StudentsID),
+FOREIGN KEY (TeacherID) REFERENCES Teachers(TeacherID),
+FOREIGN KEY (StudentsID) REFERENCES Students(StudentsID),
+
+);
+
+INSERT INTO Teachers_Students (TeacherID, StudentsID) VALUES 
+(1, 400),
+(1, 401),
+(2, 402),
+(2, 403),
+(3, 404),
+(3, 405),
+(1, 406),
+(2, 407),
+(3, 408),
+(1, 409);
+SELECT * FROM Teachers;
+
+SELECT * FROM TeacherDetails;
+
+SELECT * FROM Courses;
+
+SELECT * FROM Students;
